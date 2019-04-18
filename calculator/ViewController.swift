0c8380041f4fa1fd15be8318a2d0ad37aa985ab2
scrypnyc <alexanderlookua@gmail.com>
2019-04-18 16:51:11 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by Alexander Skrypnyk on 4/18/19.
+//  Copyright © 2019 skrypnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
