8c617216c720fde3469fdb6fa8ffee4a35e022f6
scrypnyc <alexanderlookua@gmail.com>
2019-04-19 13:18:26 +0300

    First step - UI is done

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var resultLabel: UILabel!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
   }
 
+  
 
 }
 
